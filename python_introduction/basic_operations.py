ef59432e15f7e8cacd500fac58ab1893858bfd1d
Haywhice <lalaayomideisaac@gmail.com>
2024-12-22 22:19:27 +0100

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+Add = int(number1 + number2)
+print("Addition of 10 and 15 is ", Add)
+Sub = int(number1 - number2)
+print("Subtraction of 10 and 5 is ", Sub)
+Mult = int(number1 * number2)
+print("Multiplication of 10 and 5 is ", Mult)
+
+
