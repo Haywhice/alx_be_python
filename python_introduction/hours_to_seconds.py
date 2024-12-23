65d7027183eb3761f1dfea214c2bb171ee3f9715
Haywhice <lalaayomideisaac@gmail.com>
2024-12-23 09:46:34 +0100

    Task2 and Task3

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,5 @@
+#a Python script that converts a specific number of hours into seconds. This task reinforces the concept of arithmetic operations within a practical context.
+
+hours = int(2)
+seconds = int(2 * 3600)
+print("2 hour(s) is ", seconds, "seconds.",)
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+#Use basic Python arithmetic operations and variable assignments to calculate the area of a rectangle using its length and width.
+
+length = int(10)
+width = int(5)
+area = int(length * width)
+print("The area of the rectangle is: ", area)
