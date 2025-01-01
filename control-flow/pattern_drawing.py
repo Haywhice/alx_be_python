5974fcbeac385f121540f793a2d326f85c149c3c
Haywhice <lalaayomideisaac@gmail.com>
2025-01-01 18:22:58 +0100

    Patterns with Nested Loops

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,11 +1,11 @@
 #This script will prompt the user to enter a positive integer, then use nested loops to print a square pattern of that size made of asterisks (*).
 
 size = int(input("Enter the size of the pattern: "))
-
-while size > 0:
+# Initialize row counter for the while loop
+row = 0
+while row < size:
     for i in range(size):
         print("*", end="")
-    print()
-    #if i == size:
-    break;
+    print() 
+    row += 1
 
