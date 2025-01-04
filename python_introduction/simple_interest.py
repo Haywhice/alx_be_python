daaaae1fb04759564031b0e288e3a81123aafaac
Haywhice <lalaayomideisaac@gmail.com>
2025-01-04 13:32:26 +0100

    Simple Interest Estimation

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,9 @@
 #Basic Python arithmetic operations and variable assignments to calculate the simple interest on a given principal amount, rate of interest, and time.
 
-principal = int(1000) #$1000
-rate = float(0.05) #5% annual interest rate
-time = int(3) #3 years
+principal = 1000 #$1000
+rate = 0.05 #5% annual interest rate
+time = 3 #3 years
 
-#(I = P * R * T)
-interest = float(principal * rate * time)
-print("The simple interest is: ", interest)
+#simple interest (I = P * R * T)
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
