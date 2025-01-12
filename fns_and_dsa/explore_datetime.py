f03b89dac99ca13f60b70556cdc634fa96c21a57
Haywhice <lalaayomideisaac@gmail.com>
2025-01-12 23:05:55 +0100

    date_time

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,15 @@
+#This script performs specified operations with date and time using the datetime module
+
+import datetime 
+
+def display_current_datetime():
+    "Displays the current date and time" 
+    current_date = datetime.datetime.now()
+    print("Current date and time: ", current_date.strftime("%Y-%m-%d %H:%M:%S"))
+display_current_datetime()
+    number_of_days = int(input("enter a number of days"))
+def calculate_future_date ():
+    number_of_days = int(input("enter a number of days"))
+    future_date = 
+
+
