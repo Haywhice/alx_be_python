3ea94e35d2ad8b75f82befea9988c0d36a35e128
Haywhice <lalaayomideisaac@gmail.com>
2025-01-12 15:11:11 +0100

    Arithmetic_Operations-Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,10 +9,10 @@ def perform_operation(num1, num2, operation):
     elif operation == "subtract":
         return num1 - num2
 
-    elif operation == "multiplication":
+    elif operation == "multiply":
         return num1 * num2
 
-    elif operation == "division":
+    elif operation == "divide":
         if num2 != 0:
              return num1 / num2
         else:
