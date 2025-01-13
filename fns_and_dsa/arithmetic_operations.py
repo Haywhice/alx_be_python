066a7b66b67efca28706771bb184f816628bc8ee
Haywhice <lalaayomideisaac@gmail.com>
2025-01-13 08:12:47 +0100

    Error fix

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -13,8 +13,10 @@ def perform_operation(num1, num2, operation):
         return num1 * num2
 
     elif operation == "divide":
-        if num2 != 0:
-             return num1 / num2
-        else:
+        if num2 == 0:
             return "ZeroDivisionError"
+        else:
+             return num1 / num2
+    else:
+        return "Ivalid operation"
 
